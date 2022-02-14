ac3b89239fcae3db9f02a73132bb856d0a3bb9ce
Yifan Chen <31731517+ychhhen@users.noreply.github.com>
2022-02-14 20:06:55 +0000

    Added pattern numbering

---
+++ 1.sliding-window/smallest-subarray-sum.py
@@ -0,0 +1,30 @@
+'''Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray 
+whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.'''
+
+def smallest_subarray_sum(target,nums):
+    # corner case
+    if sum(nums)<target:
+        return -1
+    
+    # space complexity: O(1)
+    windowLen, windowSum = len(nums), 0
+    indexStart = 0
+    
+    # time complexity: O(N+N) = O(N)
+    for indexEnd, num in enumerate(nums):
+        windowSum += num
+        # shorten the window interval    
+        while windowSum >= target:
+            windowLen = min(windowLen, indexEnd-indexStart+1)
+            windowSum-= nums[indexStart]
+            indexStart += 1
+
+    return windowLen
+
+def main():
+    arrs = [[2, 1, 5, 2, 3, 2],[2, 1, 5, 2, 8]]
+    target = 7
+    for nums in arrs:
+        print(smallest_subarray_sum(target,nums))
+
+main()
---
+++ 2.two-pointers/two_sum.py
@@ -0,0 +1,59 @@
+# The array is sorted
+# # Two sum problem
+
+# brute force with Time complexity O(N*LogN):
+#       iterate every elements and binary search the second element
+def two_sum_bf(target, nums):
+    # corner case ignore for now
+    for index, num1 in enumerate(nums):
+        num2 = target - num1
+
+        # Binary search num2's index        
+        left = index+1
+        right = len(nums)-1
+        num2_index = -1
+        while left<=right:
+            mid = (right-left)/2+left # Update mid
+            if num2 == nums[mid]:
+                num2_index = mid
+                break
+            elif nums2 > nums[mid]:
+                left += 1
+            elif nums2 < nums[mid]:
+                right -= 1
+        
+        if num2_index != -1: # Found num2 when num2_index is not -1
+            return [index, num2_index]
+    return [-1, -1]
+
+# Imp 1: two pointers
+def two_sum_tp(target, nums):
+    '''Two pointers at start and end'''
+    leftPointer, rightPointer = 0, len(nums)-1
+    while leftPointer < rightPointer:
+        currentSum = nums[leftPointer] + nums[rightPointer]
+        if target == currentSum: # Found the pair
+            return [leftPointer, rightPointer]
+        elif target > currentSum:
+            rightPointer -= 1
+        else:
+            leftPointer += 1
+    return [-1, -1] # Not found
+
+# Imp 2: hash table
+def two_sum_ht(target, nums):
+    '''Hash table'''
+    numsTable = {}
+    for index, num in enumerate(nums):
+        if num in numsTable:
+            return [numsTable[num], index]
+        else:
+            numsTable[target-num] = index
+    
+    return [-1, -1] # Not found
+
+
+
+
+
+
