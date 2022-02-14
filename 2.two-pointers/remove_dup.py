afe9062cf53471b779159541f6ef84c98f707844
Yifan Chen <31731517+ychhhen@users.noreply.github.com>
2022-02-14 20:35:51 +0000

    Remove duplicates

---
+++ 2.two-pointers/remove_dup.py
@@ -0,0 +1,31 @@
+'''
+See Leetcode: https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+'''
+
+# My code
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # bugFix: return value is length not the array
+        # if len(nums) == 1:
+        #     return len(nums)
+        pointer1, pointer2 = 0, 1
+        while pointer2<len(nums):
+            if nums[pointer1] == nums[pointer2]:
+                pointer2 += 1
+            else:
+                nums[pointer1+1] = nums[pointer2]
+                pointer1 += 1
+                
+        return pointer1+1
+
+# Imp with less code from the answer
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        countLen, pointer1, pointer2 = 1, 0, 0 # 1<=len(nums)
+        while pointer2 < len(nums):
+            if nums[pointer1] != nums[pointer2]:
+                countLen += 1
+                nums[pointer1+1] = nums[pointer2]
+                pointer1 += 1
+            pointer2 += 1
+        return countLen
